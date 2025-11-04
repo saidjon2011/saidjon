4f45a84d76770ebf8bb5ae769133066cfb9d47cc
murodbek060 <murodbek060>
2025-11-04 16:49:49 +0500

    lesson1

---
+++ lesson 1/lesson3.php
@@ -0,0 +1,25 @@
+<?php
+$ichimliklar = [
+    "fanta" => 15,
+    "cola" => 10,
+    "pepsi" => 20
+];
+
+asort($ichimliklar); // qiymat (son) bo‘yicha o‘sish tartibida sort qiladi
+
+foreach ($ichimliklar as $nom => $soni) {
+    echo $nom . " - " . $soni . " ta<br>";
+}
+
+$m=1;
+echo $m;
+
+?>
+
+
+
+
+
+
+
+    
---
+++ lesson 1/saidjon.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+input,button,form{
+    color:red;
+    background-color: blue;
+    text-align:center;
+}
+
+    </style>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <input type="number" name="n" placeholder="n=" >
+      <br>
+      <input type="number" name="m" placeholder="m=" >  <br>
+        <button type="submit">Yuborish</button>
+    </form>
+   
+</body>
+</html>
+
